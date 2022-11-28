d885d0beb5d23356ebd077a1297ed1c86036f7eb
huangjiaheng <huang_jia_heng@163.com>
2022-11-28 16:01:56 +0800

    [test]build test

---
+++ android/src/main/java/com/umeng/umeng_common_sdk/UmengCommonSdkPlugin.java
@@ -0,0 +1,8 @@
+package com.umeng.umeng_common_only_ios_sdk;
+
+public class UmengCommonSdkPlugin{
+
+  void initCommon(){
+
+  }
+}
--- android/src/main/java/com/umeng/umeng_common_sdk/UmengCommonSdkPlugin1.java
+++
@@ -1,8 +0,0 @@
-package com.umeng.umeng_common_only_ios_sdk;
-
-public class UmengCommonSdkPlugin1{
-
-  void initCommon1(){
-
-  }
-}
